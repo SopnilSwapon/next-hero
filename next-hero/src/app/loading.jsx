f3108da82ac204d57a00e1b17b32e1183c841cd2
SopnilSwapon <sopnilswapon1@gmail.com>
2024-07-29 16:27:41 +0600

    added a common loading page

---
+++ next-hero/src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='text-7xl text-center mt-5'>
+          Common Loading...
+        </div>
+    );
+};
+
+export default Loading;
