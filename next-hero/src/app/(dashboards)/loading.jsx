19175842544a1d871dfa605f07a854bc7a19589b
SopnilSwapon <sopnilswapon1@gmail.com>
2024-07-29 16:42:29 +0600

    added spacific loading of blogs & dashboard

---
+++ next-hero/src/app/(dashboards)/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+
+        <div className='text-7xl text-center mt-5'>
+            Dashboard Loading...
+        </div>
+    );
+};
+
+export default Loading;
---
+++ next-hero/src/app/blogs/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='text-7xl text-center mt-5'>
+          Blogs Loading...
+        </div>
+    );
+};
+
+export default Loading;
